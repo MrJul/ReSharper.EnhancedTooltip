91553cbd8055624bf9ee42ca0754ec8a1a466731
Julien Lebosquain <julien@gammajul.net>
2015-02-01 15:43:42 +0100

    Force auto-contrast on description (fixes #12)

---
+++ GammaJul.ReSharper.EnhancedTooltip/Presentation/AutoContrastedRichTextPresenter.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.UI.Avalon.Controls;
+
+namespace GammaJul.ReSharper.EnhancedTooltip.Presentation {
+
+	/// <summary>
+	/// Workaround the base <see cref="RichTextPresenter"/> which doesn't re-render its content
+	/// when <see cref="RichTextPresenter.IsAutoContrasted"/> changes (it doesn't work if the text has been set first).
+	/// </summary>
+	public class AutoContrastedRichTextPresenter : RichTextPresenter {
+
+		public AutoContrastedRichTextPresenter() {
+			IsAutoContrasted = true;
+		}
+
+	}
+
+}
