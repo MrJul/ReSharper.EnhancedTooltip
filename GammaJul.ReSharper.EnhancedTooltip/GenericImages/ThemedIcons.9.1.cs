705f1ece1bb3af5aae0b3c3ec144441b71e937eb
Julien Lebosquain <julien@gammajul.net>
2015-03-29 16:58:01 +0200

    Fixed icon for 9.1 build.

---
+++ GammaJul.ReSharper.EnhancedTooltip/GenericImages/ThemedIcons.9.1.cs
@@ -0,0 +1,9 @@
+namespace GammaJul.ReSharper.EnhancedTooltip {
+
+	public sealed partial class ThemedIcons {
+
+		internal const string IconPackResourceName = "GammaJul.ReSharper.EnhancedTooltip.9.1;component/GenericImages/ThemedIcons.Generic.Generated.Xaml";
+
+	}
+
+}
---
+++ GammaJul.ReSharper.EnhancedTooltip/Presentation/FontSizeScaleConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+namespace GammaJul.ReSharper.EnhancedTooltip.Presentation {
+
+	public class FontSizeScaleConverter : IValueConverter {
+
+		public double Scale { get; set; }
+		
+		[CanBeNull]
+		private static object ScaleDouble([CanBeNull] object value, double scale) {
+			if (!(value is double))
+				return DependencyProperty.UnsetValue;
+			return ((double) value) * scale;
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			return ScaleDouble(value, Scale);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			return ScaleDouble(value, 1.0 / Scale);
+		}
+
+		public FontSizeScaleConverter() {
+			Scale = 1.0;
+		}
+
+	}
+
+}
