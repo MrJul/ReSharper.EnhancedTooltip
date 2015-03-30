a6e985857113387771111700f54d0983757028f6
Julien Lebosquain <julien@gammajul.net>
2015-03-30 12:16:41 +0200

    Fixed extension manager conditional reference for VsIntegration. Fixes #35

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,7 @@
 "%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" Build.targets
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.8.2.nuspec
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.0.nuspec
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.1.nuspec
+mkdir Deploy\8.2
+mkdir Deploy\9.0
+mkdir Deploy\9.1
+nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.8.2.nuspec -NoPackageAnalysis -OutputDirectory Deploy\8.2
+nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.0.nuspec -NoPackageAnalysis -OutputDirectory Deploy\9.0
+nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.1.nuspec -NoPackageAnalysis -OutputDirectory Deploy\9.1
