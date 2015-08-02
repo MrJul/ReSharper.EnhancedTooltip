93e17ce8f82db6ca20147d36fb2d8f98ebb37214
Julien Lebosquain <julien@flynware.com>
2015-08-02 12:17:29 +0200

    Removed VsIntegration project, updated build files

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,5 @@
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" Build.targets
-mkdir Deploy\8.2
-mkdir Deploy\9.0
+del /Q Deploy
+del /Q bin
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Build.targets
 mkdir Deploy\9.1
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.8.2.nuspec -NoPackageAnalysis -OutputDirectory Deploy\8.2
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.0.nuspec -NoPackageAnalysis -OutputDirectory Deploy\9.0
-nuget.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.1.nuspec -NoPackageAnalysis -OutputDirectory Deploy\9.1
+packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe pack GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.9.1.nuspec -NoPackageAnalysis -OutputDirectory Deploy\9.1
--- GammaJul.ReSharper.EnhancedTooltip.VsIntegration/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("GammaJul.ReSharper.EnhancedTooltip.VsIntegration")]
-[assembly: AssemblyDescription("Integration into Visual Studio for the Enhanced Tooltip extension for ReSharper.")]
