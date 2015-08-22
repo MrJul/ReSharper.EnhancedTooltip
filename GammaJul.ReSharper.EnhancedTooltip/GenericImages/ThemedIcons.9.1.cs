0555534f96c4884d1c9f0450a25d4c9ceac51bec
Julien Lebosquain <julien@flynware.com>
2015-08-22 14:01:59 +0200

    Corrected assembly name for the options icon. Fixes #56

--- GammaJul.ReSharper.EnhancedTooltip/GenericImages/ThemedIcons.9.1.cs
+++
@@ -1,9 +0,0 @@
-namespace GammaJul.ReSharper.EnhancedTooltip {
-
-	public sealed partial class ThemedIcons {
-
-		internal const string IconPackResourceName = "GammaJul.ReSharper.EnhancedTooltip.9.1;component/GenericImages/ThemedIcons.Generic.Generated.Xaml";
-
-	}
-
-}
