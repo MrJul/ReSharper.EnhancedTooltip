bc776e55cf3f3c5ce117bd3b662b14e607270307
Julien Lebosquain <julien@flynware.com>
2015-09-17 00:08:54 +0200

    Added IdentifierPresenter to display identifier and overload count as a paragraph. Fixes #55

---
+++ GammaJul.ReSharper.EnhancedTooltip/Presentation/IdentifierPresenter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using JetBrains.Annotations;
+using JetBrains.UI.Avalon.Controls;
+using JetBrains.UI.Avalon.Interop;
+using JetBrains.UI.RichText;
+
+namespace GammaJul.ReSharper.EnhancedTooltip.Presentation {
+
+	/// <summary>
+	/// Similar to <see cref="RichTextPresenter"/>, but specialized for identifiers, with optional overload count display.
+	/// </summary>
+	public class IdentifierPresenter : TextBlock {
+
+		[NotNull]
+		public static readonly DependencyProperty IdentifierTextProperty = DependencyProperty.Register(
+			"IdentifierText",
+			typeof(RichText),
+			typeof(IdentifierPresenter),
+			new FrameworkPropertyMetadata(null, (d, e) => (d as IdentifierPresenter)?.OnRichTextChanged(e.NewValue as RichText)));
+
+		[NotNull]
+		public static readonly DependencyProperty OverloadCountProperty = DependencyProperty.Register(
+			"OverloadCount",
+			typeof(int?),
+			typeof(IdentifierPresenter),
+			new FrameworkPropertyMetadata(null, (d, e) => (d as IdentifierPresenter)?.OnOverloadCountChanged(e.NewValue as int?)));
+
+		[CanBeNull] private Span _richTextSpan;
+		[CanBeNull] private Run _overloadCountRun;
+
+		[CanBeNull]
+		public RichText IdentifierText {
+			get { return (RichText) GetValue(IdentifierTextProperty); }
+			set { SetValue(IdentifierTextProperty, value); }
+		}
+
+		public int? OverloadCount {
+			get { return (int?) GetValue(OverloadCountProperty); }
+			set { SetValue(OverloadCountProperty, value); }
+		}
+
+		private void OnRichTextChanged([CanBeNull] RichText newValue) {
+			_richTextSpan = newValue?.ToSpan();
+			SetInlines();
+		}
+
+		private void OnOverloadCountChanged(int? newValue) {
+			string text = GetOverloadCountText(newValue);
+			_overloadCountRun = text == null ? null : CreateOverloadCountRun(text);
+			SetInlines();
+		}
+
+		[CanBeNull]
+		private Run CreateOverloadCountRun([NotNull] string text)
+			=> new Run(text) {
+				Foreground = Brushes.Gray,
+				FontSize = FontSize * 0.9,
+				FontStyle = FontStyles.Italic
+			};
+
+		[CanBeNull]
+		private string GetOverloadCountText(int? overloadCount) {
+			switch (overloadCount) {
+				case null: return null;
+				case 1: return " + 1 overload";
+				default: return string.Format(CultureInfo.CurrentCulture, " + {0} overloads", overloadCount.Value);
+			}
+		}
+
+		protected override void OnInitialized(EventArgs e) {
+			base.OnInitialized(e);
+			SetInlines();
+		}
+
+		private void SetInlines() {
+			if (!IsInitialized)
+				return;
+
+			var inlines = Inlines;
+			inlines.Clear();
+			if (_richTextSpan != null)
+				inlines.Add(_richTextSpan);
+			if (_overloadCountRun != null)
+				inlines.Add(_overloadCountRun);
+		}
+
+	}
+
+}
