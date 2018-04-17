ffaf80ae99ae57352d8f46e15134772ffbcaa95d
Julien Lebosquain <julien@lebosquain.net>
2018-04-17 16:40:38 +0200

    Updated build files.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 rmdir /s /q build
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" src\GammaJul.ReSharper.EnhancedTooltip.sln /p:Platform=x86 /p:Configuration=Release
-"%USERPROFILE%\.nuget\packages\NuGet.CommandLine\4.3.0\tools\nuget.exe" pack src\GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.nuspec -NoPackageAnalysis -OutputDirectory build\Release
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" src\GammaJul.ReSharper.EnhancedTooltip.sln /p:Configuration=Release
+"%USERPROFILE%\.nuget\packages\NuGet.CommandLine\4.6.2\tools\nuget.exe" pack src\GammaJul.ReSharper.EnhancedTooltip\GammaJul.ReSharper.EnhancedTooltip.nuspec -NoPackageAnalysis -OutputDirectory build\Release
